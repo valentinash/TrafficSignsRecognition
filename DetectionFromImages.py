d9189d9ef34adacfcaeb4e0b74d3bc53598b6407
Rreze Pylla <rrezepylla@gmail.com>
2021-02-02 00:31:24 +0100

    Detection of traffic signs from images
    Dataset restructure

---
+++ DetectionFromImages.py
@@ -0,0 +1,32 @@
+import time
+import numpy as np
+from Functions import Methods
+import cv2
+from resources import Constants
+
+
+class DetectionFromImages(Methods):
+    M = Methods()
+    if __name__ == '__main__':
+        for i in Constants.ImageNamePath:
+            testCase = M.readImage(i)
+            img = np.copy(testCase)
+            try:
+                img = M.filteringImages(img)
+                img = M.returnRedness(img)
+                img = M.threshold(img, T=155)
+                img = M.morphology(img, 11)
+                contours = M.findContour(img)
+                big = M.findBiggestContour(contours)
+                testCase, sign = M.boundaryBox(testCase, big)
+                tic = time.time()
+                print("Shenja e detektuar: ", Constants.labelToText[M.predict(sign)])
+                #toc = time.time()
+                #print("Running Time of Model4", (toc - tic) * 1000, 'ms')
+                print("--------------------------------------------------------")
+                if cv2.waitKey(1) == ord('q'):
+                    break
+            except:
+                pass
+            M.show(testCase)
+            M.show(img)
---
+++ Main.py
@@ -0,0 +1,46 @@
+import os
+from tkinter import *
+from subprocess import call
+
+root = Tk()
+root.title('Detektimi i shenjave të trafikut')
+root.geometry("500x300")
+
+frame = Frame(root)
+frame.pack()
+
+y = 70
+
+
+def detectFromImg():
+    call(["python", "DetectionFromImages.py"])
+
+
+def detectFromCamera():
+    call(["python", "DetectionFromCam.py"])
+
+
+def detectFromVideo():
+    call(["python", "DetectionFromVideo.py"])
+
+
+btnFromPhotos = Button(root, text="Detektimi\nnga\nfotot", command=detectFromImg,
+                       fg="black", font="Verdana 12",
+                       bd=2, bg="light blue", relief="groove")
+btnFromPhotos.place(x=50, y=y)
+
+btnFromCamera = Button(root, text="Detektimi\nnga\nkamera", command=detectFromCamera,
+                       fg="black", font="Verdana 12",
+                       bd=2, bg="light blue", relief="groove")
+btnFromCamera.place(x=200, y=y)
+
+btnFromVideo = Button(root, text="Detektimi\nnga\nvideo", command=detectFromVideo,
+                      fg="black", font="Verdana 12",
+                      bd=2, bg="light blue", relief="groove")
+btnFromVideo.place(x=350, y=y)
+
+btnClose = Button(root, text="Close", fg="black", font="Verdana 12",
+                  bd=2, bg="red", relief="groove", compound=LEFT, command=root.destroy)
+btnClose.place(x=220, y=180)
+
+root.mainloop()
